67a5fb3d64bfa856982ad451777b2e31bce55fd1
Daniel Rajic <danielrajicc@gmail.com>
2017-01-13 13:54:17 +0100

    program changed

---
+++ src/at/htlwels/rajicd/BruchTest.java
@@ -0,0 +1,16 @@
+package at.htlwels.rajicd;
+
+public class BruchTest {
+
+    public static void main(String[] args) {
+
+        double result = divide(5,6);
+        System.out.println(result);
+
+    }
+
+    public static double divide(double zahl1, double zahl2){
+
+        return zahl1/zahl2;
+    }
+}
